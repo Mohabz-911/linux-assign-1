c77a2151153af80b0be9cf41517a34b604876381
Mohabz-911 <hoobamz@gmail.com>
2024-08-15 01:44:13 +0300

    output check

--- pwd.c
+++ pwd.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
-#define MAX_STR_LENGTH (__INT_MAX__ - 1)
+#define MAX_STR_LENGTH 1000
 
 int get_string_length(char * str)
 {
     char * ptr = str;
     int length = 0;
+
+    if(ptr == NULL) return -1;
 
     while(*ptr != '\0')
     {
@@ -16,14 +18,37 @@ int get_string_length(char * str)
     return length;
 }
 
-void main(void)
+int main(void)
 {
     char * dir_name;
-    int dir_length;
+    int dir_length, write_check;
     
-    dir_name = getcwd((void*)0, 0);
-    dir_length = get_string_length(dir_name);
+    dir_name = getcwd(NULL, 0);
+    if(dir_name == NULL)
+    {
+        write(STDOUT_FILENO, "ERROR: Couldn't get current directory name\n", 44);
+        return 0;
+    }
 
-    write(STDOUT_FILENO, dir_name, dir_length);
-    write(STDOUT_FILENO, "\n", 1);
+    dir_length = get_string_length(dir_name);
+    if(dir_length == -1)
+    {
+        write(STDOUT_FILENO, "ERROR: Directory name too long\n", 32);
+        return 0;
+    }
+
+    write_check = write(STDOUT_FILENO, dir_name, dir_length);
+    if(write_check == -1)
+    {
+        write(STDERR_FILENO, "STDOUT ERROR: Failed to write output\n", 38);
+        return 0;
+    }
+    write_check = write(STDOUT_FILENO, "\n", 1);
+    if(write_check == -1)
+    {
+        write(STDERR_FILENO, "STDOUT ERROR: Failed to write output\n", 38);
+        return 0;
+    }
+
+    return 0;
 }
